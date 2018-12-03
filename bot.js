23258b25424db0665978ed4cfbf8d11c0ffa66ff
HmaCraftYT <45132766+HmaCraftYT@users.noreply.github.com>
2018-12-03 13:01:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Play :notes: Zonix'Music | 4play`)
+client.user.setGame(`Play 🎶 Zonix'Music | 4play`)
 client.user.setStatus("online")
 });
 
